8cf4286b78bc0d5e6a43fa5b2e8b86b992eedcee
= <walker@mfgis.com>
2018-11-13 10:45:18 -0800

    name the container

--- scripts/shapes-to-volume.sh
+++ scripts/shapes-to-volume.sh
@@ -1,5 +1,4 @@
 #!/bin/sh
 docker volume create osm-shapes
-docker run -v osm-shapes:/osm-shapes --name helper busybox true
-docker cp data/. helper:/osm-shapes
-docker rm helper
+docker run -v osm-shapes:/osm-shapes --name osm-shapes-volume busybox true
+docker cp data/. osm-shapes-volume:/osm-shapes
